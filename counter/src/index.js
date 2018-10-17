8ec3aca642b017b124f950354ce7d1c151622cde
sarnickaa <sarnicka.aleks@gmail.com>
2018-10-17 12:34:57 -0400

    working solution to counter

---
+++ counter/src/index.js
@@ -0,0 +1,73 @@
+//to start server - npm 'command from package.json: scripts'
+//npm start will run webpack-dev-server --open (--open open default browser)
+
+import h from 'hyperscript'
+import hh from 'hyperscript-helpers'
+
+const {
+  div,
+  button
+} = hh(h)
+//hyperscript-helpers depends on hyperscript to run
+
+const initModel = 0
+
+//view functions:
+// RETURNS THE html and CSS to be shown based on the current statue of the app
+//model = the number that will change
+// pure functions MUST have an input paramater and return a value based on that param
+function view(dispatch, model) {
+  // return div({ className: ''}, []) 1st param object literal for CSS classes (not mandatory), next param = tags or array of tags to create
+  return div([
+    div({
+      className: 'mv2'
+    }, `Count: ${model}`),
+    button({ className: 'pv1 ph2 mr2',
+      // onclick: dispatch() - would not work as dispatch() is scoped within app function
+      // fixed by passing dispatch as first param to VIEW function
+      // makes use of currying
+      onclick: () => dispatch('plus') }, '+'),
+    button({ className: 'pv1 ph2',
+      onclick: () => dispatch('minus') }, '-')
+  ])
+}
+//onclick = an event listener on the DOM - with an event handler function passed to handle the event
+
+//updating function to increase/decrease number
+// msg = an indicaton of whether + or - was pressed i.e. a string
+function update(msg, model) {
+  switch (msg) {
+    case 'plus':
+      return model + 1;
+    case 'minus':
+      return model - 1;
+    default:
+      return model;
+  }
+}
+
+//impure code:
+function app(initModel, update, view, node) {
+  let model = initModel // holds state of the app - allows reassignment of value
+  let currentView = view(dispatch, model) // initail view set with initModel - yet can be reassigned
+  //causes side efefct of showing initial page load view
+  node.appendChild(currentView)
+
+//handles the update sequence:
+  function dispatch(msg) {
+    model = update(msg, model)
+    // reassigns model value to updated value
+    const updatedView = view(dispatch, model)
+    // creates updatedView with updated model value
+    node.replaceChild(updatedView, currentView)
+    // re-renders the view
+    currentView = updatedView
+  }
+}
+
+//rendering the view function into root HTML node
+const rootNode = document.getElementById('app')
+
+app(initModel, update, view, rootNode)
+
+// rootNode.appendChild(view(update('-', initModel)))
--- counter/webpack.config.js
+++ counter/webpack.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  // webpack config options to build the app
+  // tells webpack to start with the js code in the index folder
+  // compile that code into older versions (through babel) to older js code to make it compatible across browsers
+  // output of compilation goes in bundle.js - this will also include all the libraries used
+  // bundle.js gets pulled into the browser when webapp is built
   entry: ['./src/index.js'],
   devtool: 'inline-source-map',
   output: {
